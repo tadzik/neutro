48719f53a9225cc16f5083a81731fb0a4b333291
Tadeusz Sośnierz <tadzikes@gmail.com>
2011-01-08 13:00:24 +0100

    Removed --notest from bootstrap.sh, snarkyboojum++, heisenbugs--

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,4 +3,4 @@ git clone --depth 1 git://github.com/tadzik/perl6-Module-Tools.git mt
 git clone --depth 1 git://github.com/tadzik/perl6-File-Tools.git ft
 git clone --depth 1 git://github.com/moritz/json.git jt
 
-PERL6LIB=mt/lib:ft/lib:jt/lib bin/neutro . --v --notest
+PERL6LIB=mt/lib:ft/lib:jt/lib bin/neutro .
