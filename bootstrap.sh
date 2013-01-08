758b43a9298c7a4081a98ceca49deec607167da1
Tadeusz Sośnierz <tadzikes@gmail.com>
2013-01-08 01:07:46 +0100

    Make it clear that the project is discontinued

--- bootstrap.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git clone --depth 1 git://github.com/tadzik/perl6-Module-Tools.git mt
-git clone --depth 1 git://github.com/tadzik/perl6-File-Tools.git ft
-
-PERL6LIB=mt/lib:ft/lib bin/neutro .
